1da39f4b6fdeebb96028f1d7d031abb7fb419dbd
Devin Samarin <eboyjr14@gmail.com>
2011-05-05 22:08:51 -0700

    Fix TextLayout to do a reasonable job of word-wrapping; Create tests/ subdir for tests

--- javascript/TextLayout.js
+++ javascript/TextLayout.js
@@ -1,7 +1,7 @@
 var TextLayout = function(context) {
 	this.context = context;
 	this.text = "";
-	this.text_split = [""];
+	this.text_wrapped = [""];
 	
 	this.font_name = null;
 	this.font_size = null;
@@ -13,6 +13,9 @@ var TextLayout = function(context) {
 	this.align = "left";
 	this.width = null; // Infinite
 	this.calculated = true;
+	
+	this.apparent_width = null;
+	this.apparent_height = null;
 };
 
 TextLayout.canvas_context = (function() {
@@ -22,8 +25,9 @@ TextLayout.canvas_context = (function() {
 
 TextLayout.prototype.text_width = function(string) {
 	var cc = TextLayout.canvas_context;
-	cc.font = this.font;
+	//cc.font = this.font;
 	return cc.measureText(string).width;
+};
 
 TextLayout.prototype.set_text = function (text) {
 	this.text = text;
@@ -49,23 +53,53 @@ TextLayout.prototype.recalculate_layout = function() {
 	var line_split = this.text.split("\n"); // TODO: Regex
 	var number_of_lines = line_split.length;
 	
-	var line_sizes = new Array(number_of_lines);
-	var word_sizes = new Array(number_of_lines);
-	for (var i = 0; i < number_of_lines; i++) {
-		line_sizes[i] = this.text_width(line_split[i]);
+	TextLayout.canvas_context.font = this.font_size + "px \""+this.font_name+"\"";
+	
+	var text_wrapped = [];
+	
+	if (this.width === null) { // No bounding width on layout
+		var apparent_width = 0;
+		for (var i = 0; i < number_of_lines; i++) {
+			apparent_width = Math.max(apparent_width,
+				this.text_width (line_split[i]));
+		}
+		this.apparent_width = apparent_width;
+		this.apparent_height = this.font_size * number_of_lines;
 		
-		var word_split = line_split[i].split(" "); // TODO: Regex
-		var number_of_words = word_split.length;
+		text_wrapped = line_split;
 		
-		word_sizes[i] = new Array(number_of_words);
-		for (var j = 0; j < number_of_words; j++) {
-			word_sizes[i][j] = this.text_width(word_split[j]);
+	} else { // Constrained to width
+		for (var i = 0; i < number_of_lines; i++) {
+		
+			var line = line_split[i];
+		
+			var start_index = 0;
+			var break_index = 0;
+			var last_break_index = 0;
+			
+			var m, w;
+			
+			while (m = line.substr(last_break_index).match(/. |-[^ ]/)) {
+				break_index += m.index+1;
+				
+				if ((w = this.text_width(line.substring(start_index, break_index)))
+					> this.width) {
+					
+					var wrap_line = line.substring(start_index, last_break_index);
+					if (start_index !== 0) wrap_line = wrap_line.replace(/^\s+/, "");
+					
+					text_wrapped.push(wrap_line);
+					start_index = last_break_index;
+					
+				}
+				last_break_index = break_index;
+			
+			}
+			
+			text_wrapped.push(line.substring(start_index).replace(/^\s+/, ""));
+			
 		}
 	}
-	
-	this.line_split = line_split;
-	this.line_sizes = line_sizes;
-	this.word_sizes = word_sizes;
-	this.space_width = this.text_width(" ");
 	this.calculated = true;
+	this.text_wrapped = text_wrapped;
 };
